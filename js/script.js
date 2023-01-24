715ccf990269ed479d7c29684912a120629e89f0
levi7x <117045024+levi7x@users.noreply.github.com>
2023-01-24 01:15:09 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,94 +1,96 @@
 const header = document.querySelector("header");
-window.addEventListener("scroll", function() {
-    header.classList.toggle("sticky", window.scrollY > 100)
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 100);
 });
 
 let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
-menu.onclick = () =>{
-    menu.classList.toggle("bx-x");
-    navbar.classList.toggle("open");
-}
-
-window.onscroll = () =>{
-    menu.classList.remove("bx-x");
-    navbar.classList.remove("open");
-}
-
-/* https://smtpjs.com/ CA591BAF44820377DBB51891DCF9318ED5FF*/
-function sendEmail(){
-        Email.send({
-            SecureToken : "9a8b254f-ef5d-4441-bd12-a786857bcc6a",
-            To : 'simomarian99@gmail.com',
-            From : 'simomarian99@gmail.com',
-            Subject : document.getElementById("email").value,
-            Body : '<h1>' + document.getElementById("subject").value + '</h1>' + ' :' +  document.getElementById("message").value,
-        }).then(
-          message => alert(message)
-        );
-        console.log('EMAIL SENT');
-}
-
-
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("open");
 };
 
-TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navbar.classList.remove("open");
+};
 
-    if (this.isDeleting) {
+/* https://smtpjs.com/ CA591BAF44820377DBB51891DCF9318ED5FF*/
+function sendEmail() {
+  Email.send({
+    SecureToken: "9a8b254f-ef5d-4441-bd12-a786857bcc6a",
+    To: "simomarian99@gmail.com",
+    From: "simomarian99@gmail.com",
+    Subject: document.getElementById("email").value,
+    Body:
+      "<h1>" +
+      document.getElementById("subject").value +
+      "</h1>" +
+      " :" +
+      document.getElementById("message").value,
+  }).then((message) => alert(message));
+  console.log("EMAIL SENT");
+}
+
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
     this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
+  } else {
     this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
+  }
 
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
 
-    var that = this;
-    var delta = 200 - Math.random() * 100;
+  var that = this;
+  var delta = 200 - Math.random() * 100;
 
-    if (this.isDeleting) { delta /= 2; }
+  if (this.isDeleting) {
+    delta /= 2;
+  }
 
-    if (!this.isDeleting && this.txt === fullTxt) {
+  if (!this.isDeleting && this.txt === fullTxt) {
     delta = this.period;
     this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
+  } else if (this.isDeleting && this.txt === "") {
     this.isDeleting = false;
     this.loopNum++;
     delta = 500;
-    }
+  }
 
-    setTimeout(function() {
+  setTimeout(function () {
     that.tick();
-    }, delta);
+  }, delta);
 };
 
-window.onload = function() {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
-        }
+window.onload = function () {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
     }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-    document.body.appendChild(css);
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
 };
-
-
 
 /* var language = {
     en: {
@@ -128,3 +130,52 @@ links.forEach(link => {
  */
 
 
+var modal_eshop = document.getElementById("modal-eshop");
+var modal_runners = document.getElementById("modal-runners");
+var modal_meetups = document.getElementById("modal-meetups");
+var modal_poker = document.getElementById("modal-poker");
+var modal_villa = document.getElementById("modal-villa");
+
+var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
+var btn4 = document.getElementById("myBtn4");
+var btn5 = document.getElementById("myBtn5");
+
+btn.onclick = function () {
+  modal_eshop.style.display = "block";
+};
+
+btn2.onclick = function () {
+  modal_runners.style.display = "block";
+};
+
+btn3.onclick = function () {
+  modal_meetups.style.display = "block";
+};
+
+btn4.onclick = function () {
+  modal_poker.style.display = "block";
+};
+
+btn5.onclick = function () {
+  modal_villa.style.display = "block";
+};
+
+function closeModal(){
+    modal_runners.style.display = "none";
+    modal_eshop.style.display = "none";
+    modal_meetups.style.display = "none";
+    modal_poker.style.display = "none";
+    modal_villa.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target == modal_eshop || event.target == modal_runners || event.target == modal_meetups || event.target == modal_poker || event.target == modal_villa) {
+    modal_eshop.style.display = "none";
+    modal_runners.style.display = "none";
+    modal_meetups.style.display = "none";
+    modal_poker.style.display = "none";
+    modal_villa.style.display = "none";
+  }
+};
